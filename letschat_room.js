e6585d73e858b5e7dc1683ea1c3a62b7a72c3699
Shaurya-Yay <73387390+Shaurya-Yay@users.noreply.github.com>
2021-05-04 14:27:36 +0530

    updated

--- letschat_room.js
+++ letschat_room.js
@@ -22,3 +22,23 @@ function addRoom() {
     localStorage.setItem("room_name", room_name);
     window.location = "index.html";
 }
+
+function getData() {
+    firebase.database().ref("/").on('value', function(snapshot) {
+        document.getElementById("output").innerHTML = "";
+        snapshot.forEach(function(childSnapshot) {
+            childKey = childSnapshot.key;
+            Room_names = childKey;
+            console.log("Room Name - " + Room_names);
+            row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+            document.getElementById("output").innerHTML = row;
+        });
+    });
+}
+getData();
+
+function redirectToRoomName(name) {
+    console.log(name);
+    localStorage.setItem("room_name", name);
+    window.location = "";
+}
